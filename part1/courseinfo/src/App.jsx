ae8142428adaf7dd4653db354575ea280b9c6a41
fbponz <fbponz@gmail.com>
2024-03-03 18:40:33 +0100

    Exercise "1.3: información del curso, paso 3" https://fullstackopen.com/es/part1/java_script#ejercicios-1-3-1-5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,29 +8,41 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.title} {props.numExercices}</p>
+      <p>{props.part['name']} {props.part['exercises']}</p>
     </div>
   )
 }
-
+const Totals = (props) => {
+  return (
+    <div>
+      <p>{props.title} {props.numExercices}</p>
+    </div>
+  )  
+}
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header title={course} />
-      <Content title={part1} numExercices={exercises1} />
-      <Content title={part2} numExercices={exercises2} />
-      <Content title={part3} numExercices={exercises3} />
-      <Content title='Number of exercises' numExercices={exercises1 + exercises2 + exercises3} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Totals title='Number of exercises' numExercices={part1.exercises + part2.exercises2 + part3.exercises3} />
     </div>
   )
 }
